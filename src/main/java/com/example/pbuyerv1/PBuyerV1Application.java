231aeff95d2897536cc6f8e01c1b2b7ed2cf3614
SCHcoding <codingb007@gmail.com>
2024-05-01 10:31:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/pbuyerv1/PBuyerV1Application.java
@@ -0,0 +1,13 @@
+package com.example.pbuyerv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PBuyerV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PBuyerV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pbuyerv1/PBuyerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbuyerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PBuyerV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
